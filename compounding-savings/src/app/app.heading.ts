e6ff273bacf334527d419cac92a328d36822d0c5
Harvey <harveydelaney@yahoo.com.au>
2017-04-16 17:00:49 +1000

    got basic design setup

--- compounding-savings/src/app/app.heading.ts
+++ compounding-savings/src/app/app.heading.ts
@@ -3,17 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template:`
-    <h1>
-      {{title}}
-    </h1>  
-    <button (click)="clicked()"> Click Me </button> 
+    <div class="row">
+      <div class="col-md-12">
+        <h1 class="mainHeaderText">
+          {{title}}
+        </h1> 
+      </div>
+    </div>
   `,
   styleUrls: ['./app.component.css']
 })
 export class HeadingComponent {
-  title = 'app works!';
-  
-  clicked(){
-    alert("hello");
-  }
+  title = 'Savings Motivator';
 }
