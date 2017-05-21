a6f41682e1b17ce1ccd7433b3bacdf7b263caf86
Harvey <harveydelaney@yahoo.com.au>
2017-05-21 22:54:04 +1000

    significantly reduced bundle file size

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,43 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    devServer: {
-        inline: true,
-        contentBase: './src',
-        port: 3000
-    },
-    devtool: 'cheap-module-eval-source-map',
+    // devServer: {
+    //     inline: true,
+    //     contentBase: './src',
+    //     port: 3000
+    // },
+    //devtool: 'cheap-module-eval-source-map',
+    devtool: 'source-map',
     entry: './dev/js/index.js',
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loaders: ['babel'],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.scss/,
-                loader: 'style-loader!css-loader!sass-loader'
-            }
-        ]
-    },
     output: {
         path: 'src',
         filename: 'js/bundle.min.js'
     },
     plugins: [
-        new webpack.optimize.OccurrenceOrderPlugin()
-    ]
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+                compressor: {
+                    warnings: false
+                }
+            })
+    ],
+    module: {
+    loaders: [
+        {
+            test: /\.js$/,
+            loaders: ['babel'],
+            exclude: /node_modules/
+        },
+        {
+            test: /\.scss/,
+            loader: 'style-loader!css-loader!sass-loader',
+            exclude: /node_modules/
+        }
+     ]
+    }
 };
