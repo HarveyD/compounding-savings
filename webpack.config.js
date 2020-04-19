0ab2fecfb09d24b936981af82259c27bc54b3892
Harvey Delaney <harvey@harveydelaney.com>
2020-04-19 14:07:27 +1000

    fix: update this very old project to work with new CI

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,43 @@
-var path = require('path');
-var webpack = require('webpack');
+var webpack = require("webpack");
 
 module.exports = {
-    // devServer: {
-    //     inline: true,
-    //     contentBase: './src',
-    //     port: 3000
-    // },
-    //devtool: 'cheap-module-eval-source-map',
-    devtool: 'source-map',
-    entry: './dev/js/index.js',
-    output: {
-        path: 'src',
-        filename: 'js/bundle.min.js'
-    },
-    plugins: [
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-                compressor: {
-                    warnings: false
-                }
-            })
-    ],
-    module: {
+  // devServer: {
+  //     inline: true,
+  //     contentBase: './src',
+  //     port: 3000
+  // },
+  //devtool: 'cheap-module-eval-source-map',
+  devtool: "source-map",
+  entry: "./dev/js/index.js",
+  output: {
+    filename: "bundle.min.js",
+    path: __dirname + "/build"
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ],
+  module: {
     loaders: [
-        {
-            test: /\.js$/,
-            loaders: ['babel'],
-            exclude: /node_modules/
-        },
-        {
-            test: /\.scss/,
-            loader: 'style-loader!css-loader!sass-loader',
-            exclude: /node_modules/
-        }
-     ]
-    }
+      {
+        test: /\.js$/,
+        loaders: ["babel"],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss/,
+        loader: "style-loader!css-loader!sass-loader",
+        exclude: /node_modules/
+      }
+    ]
+  }
 };
